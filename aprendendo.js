27a97bfae7eef13a1167e4be4136a1e1cf6405d6
Silas Ribeiro <silass@gmail.com>
2021-02-07 19:42:42 -0400

    Definindo models e criando Collections

--- aprendendo.js
+++ aprendendo.js
@@ -1,12 +1,56 @@
 const mongoose = require("mongoose")
 
 //Configurando o mongoose
-mongoose.Promise = global.Promise;
+    //mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/aprendendo", {
-    useMongoClient: true
+    //useMongoClient: true
 }).then(() =>  {
 console.log("MongoDB Conectado...");
 
 }).catch((err) => {
     console.log("Houve um erro ao se conectar ao MongoDB"+err);
 })
+
+//Definir Model - Usuarios 
+
+const UsuariosSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    }
+})
+// Collection
+    mongoose.model('usuarios', UsuariosSchema )
+
+const Silas = mongoose.model('usuarios')
+//Criar User
+new Silas({
+    nome: "Paulo",
+    sobrenome: "Silveira",
+    email: "teste2@email.com",
+    idade: 51,
+    pais: "Brasil"
+}).save().then(() =>{
+    console.log("Usuario criado com Sucesso");
+}).catch((err) => {
+    console.log("Erro ao registrar o Usuario"+err);
+})
+
+// .save para salvar o usuario
+
+
